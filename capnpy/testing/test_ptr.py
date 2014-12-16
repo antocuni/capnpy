f68257da0116f2ff899788950d85f1b9f7de28d0
Antonio Cuni <anto.cuni@gmail.com>
2014-12-16 10:44:09 +0100

    add missing tests for ptr.py

--- capnpy/testing/test_ptr.py
+++ capnpy/testing/test_ptr.py
@@ -1,5 +1,22 @@
 import struct
-from capnpy.ptr import PtrStruct, PtrList
+from capnpy.ptr import Ptr, PtrStruct, PtrList
+
+def test_Ptr_generic():
+    ptr = Ptr(0x0004000200000190)
+    assert ptr.kind == PtrStruct.KIND
+    assert ptr.offset == 100
+    ptr2 = ptr.specialize()
+    assert isinstance(ptr2, PtrStruct)
+    #
+    ptr = Ptr(0x0000064700000101)
+    ptr2 = ptr.specialize()
+    assert isinstance(ptr2, PtrList)
+
+def test_Ptr_deref():
+    ptr = Ptr(0x0004000200000190)
+    assert ptr.offset == 100
+    offset = ptr.deref(8)
+    assert offset == 816
 
 def test_PtrStruct():
     #       0004             ptrs size
@@ -12,6 +29,7 @@ def test_PtrStruct():
     assert ptr.offset == 100
     assert ptr.data_size == 2
     assert ptr.ptrs_size == 4
+    assert ptr.get_size() == (2, 4)
 
 def test_PtrStruct_new():
     ptr = PtrStruct.new(100, 2, 4)
@@ -41,3 +59,4 @@ def test_PtrList_new():
     assert ptr.size_tag == 7
     assert ptr.item_count == 200
     assert ptr == 0x0000064700000101
+
