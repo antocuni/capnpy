14bc7e2107ff31b67b2434d7afead0d247f7a9c1
Antonio Cuni <anto.cuni@gmail.com>
2014-12-31 11:31:55 +0100

    add a passing test for string fields

--- capnpy/testing/test_compiler.py
+++ capnpy/testing/test_compiler.py
@@ -14,12 +14,29 @@ def test_simple_struct(tmpdir):
         y @1 :Int64;
     }
     """
+    mod = compile_string(tmpdir, schema)
+    #
     buf = ('\x01\x00\x00\x00\x00\x00\x00\x00'  # 1
            '\x02\x00\x00\x00\x00\x00\x00\x00') # 2
-    #
-    mod = compile_string(tmpdir, schema)
     p = mod.Point.from_buffer(buf)
     assert p.x == 1
     assert p.y == 2
 
-    
+
+def test_string(tmpdir):
+    schema = """
+    @0xbf5147cbbecf40c1;
+    struct Foo {
+        x @0 :Int64;
+        name @1 :Text;
+    }
+    """
+    mod = compile_string(tmpdir, schema)
+
+    buf = ('\x01\x00\x00\x00\x00\x00\x00\x00'   # 1
+           '\x01\x00\x00\x00\x82\x00\x00\x00'   # ptrlist
+           'hello capnproto\0')                 # string
+
+    f = mod.Foo.from_buffer(buf)
+    assert f.x == 1
+    assert f.name == 'hello capnproto'
