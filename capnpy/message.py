a9192ca6c99f2b6de558836e644376f74ec1175a
Antonio Cuni <anto.cuni@gmail.com>
2015-01-05 23:10:02 +0100

    completely refactor how messages are interpreted, I got it wrong earlier; the first bytes represent the segment count and sizes

--- capnpy/message.py
+++ capnpy/message.py
@@ -1,41 +1,39 @@
+import struct
 from capnpy.blob import Blob
 
 def loads(buf, payload_type):
     """
-    Decode the capnp message in buf, using the given payload type.
-    payload_type is expected to be a struct.
+    Load a message of type ``payload_type`` from buf.
+
+    The message is encoded using the recommended capnp format for serializing
+    messages over a stream:
+
+      - (4 bytes) The number of segments, minus one (since there is always at
+        least one segment)
+    
+      - (N * 4 bytes) The size of each segment, in words.
+    
+      - (0 or 4 bytes) Padding up to the next word boundary.
+
+      - The content of each segment, in order.
     """
-    m = Message(buf)
-    return m.get_struct(payload_type)
-
-
-class Message(object):
-    """
-    Represent a full capnp message.
-
-    Glossary:
-
-      - Message: the whole array-of-bytes; it starts with a pointer to an
-        anonymous struct with only one field, called ``root``
-
-      - root: the struct whose only field contains the actual payload; most of
-        the time, it's a pointer to a struct, but we don't know its type
-        statically
-
-      - payload: the data contained in the root's field
-
-    The various get_* methods are used to interpret the payload as
-    required. For example, if we know the payload is a struct, we will use
-    get_struct(). Since capnp does not support introspection, there is no way
-    to query the type of the payload in case we don't know it.
-    """
-
-    def __init__(self, buf):
-        blob = Blob.from_buffer(buf, 0)
-        self._root = blob._read_struct(0, Blob)
-
-    def get_struct(self, structcls):
-        """
-        Read the payload as if it were a pointer to ``structcls``
-        """
-        return self._root._read_struct(0, structcls)
+    offset = 0
+    # total number of segments
+    n = struct.unpack_from('<I', buf, offset)[0] + 1
+    offset += 4
+    fmt = '<' + ('I'*n)
+    segments = struct.unpack_from(fmt, buf, offset) # size of each segment
+    offset += 4*n
+    #
+    # add enough padding so that the message starts at word boundary
+    if offset % 8 != 0:
+        padding = 8-(offset % 8)
+        offset += padding
+    #
+    total_size = sum(segments)*8 + offset
+    if len(buf) != total_size:
+        raise ValueError("The length of the buffer does not correspond to the length of "
+                         "the segments %s: expected %s, got %s" %
+                         (segments, total_size, len(buf)))
+    msg = Blob.from_buffer(buf, offset)
+    return msg._read_struct(0, payload_type)
--- capnpy/testing/test_message.py
+++ capnpy/testing/test_message.py
@@ -1,17 +1,21 @@
-from capnpy.message import Message, loads
+import py
+from capnpy.message import loads
 from capnpy.blob import Blob, Types
 
-def test_Message():
-    buf = ('\x00\x00\x00\x00\x02\x00\x00\x00'   # ptr to anonymous struct
+def test_loads():
+    buf = ('\x00\x00\x00\x00\x03\x00\x00\x00'   # message header: 1 segment, size 3 words
            '\x00\x00\x00\x00\x02\x00\x01\x00'   # ptr to payload (Point {x, y})
            '\x01\x00\x00\x00\x00\x00\x00\x00'   # x == 1
            '\x02\x00\x00\x00\x00\x00\x00\x00')  # y == 2
-    m = Message(buf)
-    p = m.get_struct(Blob)
-    assert p._read_primitive(0, Types.int64) == 1
-    assert p._read_primitive(8, Types.int64) == 2
-    #
+
     p = loads(buf, Blob)
     assert isinstance(p, Blob)
     assert p._read_primitive(0, Types.int64) == 1
     assert p._read_primitive(8, Types.int64) == 2
+
+def test_wrong_size():
+    buf = ('\x00\x00\x00\x00\x04\x00\x00\x00'   # message header: 1 segment, size 4 words
+           '\x00\x00\x00\x00\x02\x00\x01\x00'   # ptr to payload (Point {x, y})
+           '\x01\x00\x00\x00\x00\x00\x00\x00'   # x == 1
+           '\x02\x00\x00\x00\x00\x00\x00\x00')  # y == 2
+    py.test.raises(ValueError, "loads(buf, Blob)")
