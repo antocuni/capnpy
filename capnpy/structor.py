2c553dde3c9de875ace2f905d4106c0cf73f9fb5
Antonio Cuni <anto.cuni@gmail.com>
2015-01-27 14:56:00 +0100

    a class if overweight, just make a function

--- capnpy/structor.py
+++ capnpy/structor.py
@@ -7,42 +7,33 @@ from pypytools.codegen import Code
 from capnpy import field
 from capnpy.builder import StructBuilder
 
-class Structor(object):
+def structor(name, data_size, ptrs_size, fields):
+    fmt = compute_format(data_size, ptrs_size, fields)
+    return make_structor(name, fields, fmt)
 
-    def __init__(self, data_size, ptrs_size, fields):
-        self.data_size = data_size
-        self.ptrs_size = ptrs_size
-        self.fields = fields
-        self.fmt = self.compute_format()
-        self._call = self.make_call()
+def compute_format(data_size, ptrs_size, fields):
+    total_length = (data_size+ptrs_size)*8
+    fmt = ['x'] * total_length
+    for f in fields:
+        if isinstance(f, field.Primitive):
+            fmt[f.offset] = f.type.fmt
+            for i in range(f.offset+1, f.offset+f.type.calcsize()):
+                fmt[i] = None
+        else:
+            assert False
+    #
+    # remove all the Nones
+    fmt = [ch for ch in fmt if ch is not None]
+    fmt = ''.join(fmt)
+    assert struct.calcsize(fmt) == total_length
+    return fmt
 
-    def compute_format(self):
-        total_length = (self.data_size+self.ptrs_size)*8
-        fmt = ['x'] * total_length
-        for f in self.fields:
-            if isinstance(f, field.Primitive):
-                fmt[f.offset] = f.type.fmt
-                for i in range(f.offset+1, f.offset+f.type.calcsize()):
-                    fmt[i] = None
-            else:
-                assert False
-        #
-        # remove all the Nones
-        fmt = [ch for ch in fmt if ch is not None]
-        fmt = ''.join(fmt)
-        assert struct.calcsize(fmt) == total_length
-        return fmt
-
-    def make_call(self):
-        argnames = ['arg%d' % i for i in range(len(self.fields))]
-        code = Code()
-        code['StructBuilder'] = StructBuilder
-        with code.def_('call', argnames):
-            code.w('builder = StructBuilder({fmt})', fmt=repr(self.fmt))
-            code.w('return', code.call('builder.build', argnames))
-        code.compile()
-        return code['call']
-
-
-    def __call__(self, *args):
-        return self._call(*args)
+def make_structor(name, fields, fmt):
+    argnames = ['arg%d' % i for i in range(len(fields))]
+    code = Code()
+    code['StructBuilder'] = StructBuilder
+    with code.def_(name, argnames):
+        code.w('builder = StructBuilder({fmt})', fmt=repr(fmt))
+        code.w('return', code.call('builder.build', argnames))
+    code.compile()
+    return code[name]
--- capnpy/testing/test_structor.py
+++ capnpy/testing/test_structor.py
@@ -1,24 +1,24 @@
-from capnpy.structor import Structor
+from capnpy.structor import structor, compute_format
 from capnpy import field
 from capnpy.type import Types
+
+def test_compute_format_simple():
+    fields = [field.Primitive(0, Types.int64),
+              field.Primitive(8, Types.int64)]
+    fmt = compute_format(data_size=2, ptrs_size=0, fields=fields)
+    assert fmt == 'qq'
+
+def test_compute_format_holes():
+    fields = [field.Primitive(0, Types.int32),
+              field.Primitive(8, Types.int64)]
+    fmt = compute_format(data_size=2, ptrs_size=0, fields=fields)
+    assert fmt == 'ixxxxq'
+
 
 def test_simple():
     fields = [field.Primitive(0, Types.int64),
               field.Primitive(8, Types.int64)]
-    ctor = Structor(data_size=2, ptrs_size=0, fields=fields)
-    assert ctor.fmt == 'qq'
-
-def test_holes():
-    fields = [field.Primitive(0, Types.int32),
-              field.Primitive(8, Types.int64)]
-    ctor = Structor(data_size=2, ptrs_size=0, fields=fields)
-    assert ctor.fmt == 'ixxxxq'
-
-
-def test_call():
-    fields = [field.Primitive(0, Types.int64),
-              field.Primitive(8, Types.int64)]
-    ctor = Structor(data_size=2, ptrs_size=0, fields=fields)
+    ctor = structor('ctor', data_size=2, ptrs_size=0, fields=fields)
     buf = ctor(1, 2)
     assert buf == ('\x01\x00\x00\x00\x00\x00\x00\x00'  # 1
                    '\x02\x00\x00\x00\x00\x00\x00\x00') # 2
