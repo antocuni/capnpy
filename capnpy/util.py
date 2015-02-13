5837ebd7b2dadd97836a2265effc92886c3e219c
Antonio Cuni <anto.cuni@gmail.com>
2015-02-13 17:17:50 +0100

    return the original class

--- capnpy/util.py
+++ capnpy/util.py
@@ -1,7 +1,7 @@
 def extend(original_class):
     def decorator(new_class):
         for key, value in new_class.__dict__.iteritems():
-            if key not in ('__dict__', '__doc__'):
+            if key not in ('__dict__', '__doc__', '__module__', '__weakref__'):
                 setattr(original_class, key, value)
-        return None
+        return original_class
     return decorator
