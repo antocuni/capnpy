398773c1b4124e717427d5af26b8d9bdf1b99ddc
Antonio Cuni <anto.cuni@gmail.com>
2015-07-10 18:44:27 +0200

    remove capnpy.speedups: it will be rewritten in Cython

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from distutils.core import setup, Extension
 
-speedups_ext = Extension("capnpy.speedups",
-                         ["capnpy/speedups/speedups.c",
-                          "capnpy/speedups/baseblob.c",
-                          "capnpy/speedups/primitive_field.c"])
 
 setup(name="capnpy", version="0.0",
-      ext_modules = [speedups_ext])
+      ext_modules = [])
