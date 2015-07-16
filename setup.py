b9436fcd736218b41a5e869d986c13200120973b
Antonio Cuni <anto.cuni@gmail.com>
2015-07-16 17:26:56 +0200

    compile to cython only if explicitly requested

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
+import os
 from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
 
-files = ["capnpy/blob.py",
-         "capnpy/field.py",
-         "capnpy/unpack.pyx"]
+USE_CYTHON = int(os.environ.get('USE_CYTHON', '0'))
 
-def getext(fname):
-    extname = fname.replace('/', '.').replace('.pyx', '').replace('.py', '')
-    return Extension(extname, [fname])
+if USE_CYTHON:
+    from Cython.Build import cythonize
 
-ext_modules = cythonize(map(getext, files), gdb_debug=True)
+    files = ["capnpy/blob.py",
+             "capnpy/field.py",
+             "capnpy/unpack.pyx"]
 
+    def getext(fname):
+        extname = fname.replace('/', '.').replace('.pyx', '').replace('.py', '')
+        return Extension(extname, [fname])
+
+    ext_modules = cythonize(map(getext, files), gdb_debug=True)
+
+else:
+    ext_modules = []
 
 setup(name="capnpy",
       version="0.1",
       packages = find_packages(),
       package_data = {
-          'capnpy': ['*.capnp']
+          'capnpy': ['*.capnp', '*.pyx']
           },
       ext_modules = ext_modules)
