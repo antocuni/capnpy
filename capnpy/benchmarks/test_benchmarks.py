6819b10439df58f82454935ab8f6b04268b2b197
Antonio Cuni <anto.cuni@gmail.com>
2015-10-14 01:20:36 +0200

    add benchmarks for unpack_primitive and Blob._read_primitive; for now, don't use uint64 as they are incorrectly turned into Python longs by Cython

--- capnpy/benchmarks/test_benchmarks.py
+++ capnpy/benchmarks/test_benchmarks.py
@@ -2,6 +2,7 @@ import pytest
 import pypytools
 from collections import namedtuple
 import capnpy
+
 #schema = capnpy.load_schema('/capnpy/benchmarks/point.capnp')
 
 # XXX
@@ -12,14 +13,14 @@ from capnpy.enum import enum
 from capnpy.blob import Types
 class schema:    
     class Point(Struct):
-        x = field.Primitive("x", 0, Types.uint64, default_=0)
-        y = field.Primitive("y", 8, Types.uint64, default_=0)
+        x = field.Primitive("x", 0, Types.int64, default_=0)
+        y = field.Primitive("y", 8, Types.int64, default_=0)
 
         __data_size__ = 3
         __ptrs_size__ = 1
         
         def __new__(cls, x, y):
-            builder = StructBuilder('QQ')
+            builder = StructBuilder('qq')
             buf = builder.build(x, y)
             offset = 0
             segment_offsets = None
@@ -56,17 +57,51 @@ class points(object):
 def Point(request):
     return getattr(points, request.param)
 
-@pytest.mark.benchmark(group="getattr")
-def test_getattr(benchmark, Point):
-    N = 2000
-    @pypytools.clonefunc
-    def sum_xs(p):
-        res = 0
-        for i in range(N):
-            res += p.x
-        return res
-    #
-    p = Point(x=100, y=200)
-    res = benchmark(sum_xs, p)
-    assert res == 100*N
+class TestGetAttr(object):
 
+    N = 2000
+
+    @pytest.mark.benchmark(group="getattr")
+    def test_unpack_primitive(self, benchmark):
+        """
+        Measure the raw performance of unpack_primitive, i.e. how fast we can
+        unpack Python strings into int64
+        """
+        import struct
+        from capnpy.unpack import unpack_primitive
+        #
+        def sum_xs(buf):
+            res = 0
+            for i in range(self.N):
+                res += unpack_primitive('q', buf, 0)
+            return res
+        #
+        buf = struct.pack('q', 100)
+        res = benchmark(sum_xs, buf)
+        assert res == 100*self.N
+
+    @pytest.mark.benchmark(group="getattr")
+    def test__read_primitive(self, benchmark):
+        def sum_xs(p):
+            res = 0
+            for i in range(self.N):
+                res += p._read_primitive(0, Types.uint64)
+            return res
+        #
+        p = schema.Point(x=100, y=200)
+        res = benchmark(sum_xs, p)
+        assert res == 100*self.N
+
+
+    @pytest.mark.benchmark(group="getattr")
+    def test_getattr(self, benchmark, Point):
+        @pypytools.clonefunc
+        def sum_xs(p):
+            res = 0
+            for i in range(self.N):
+                res += p.x
+            return res
+        #
+        p = Point(x=100, y=200)
+        res = benchmark(sum_xs, p)
+        assert res == 100*self.N
--- capnpy/unpack.pyx
+++ capnpy/unpack.pyx
@@ -5,10 +5,16 @@ cpdef unpack_primitive(char* sfmt, bytes buf, int offset):
     cdef void* valueaddr
     cdef long value
     cdef char fmt = sfmt[0]
+    #
+    cbuf = buf
+    valueaddr = cbuf + offset
     if fmt == 'q':
-        cbuf = buf
-        valueaddr = cbuf + offset
-        value = (<long*>valueaddr)[0]
-        return value
+        return (<long*>valueaddr)[0]
+    elif fmt == 'Q':
+        # XXX: this return a Python long: fix it to return a long only if
+        # strictly necessary
+        return (<unsigned long*>valueaddr)[0]
+    #
+    # slow fallback
     return struct.unpack_from('<' + sfmt, buf, offset)[0]
     #raise ValueError('unknown fmt %s' % chr(fmt))
