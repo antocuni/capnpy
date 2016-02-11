0560e35824ace09996f930572520ecb00c6fcb27
Antonio Cuni <anto.cuni@gmail.com>
2016-02-11 11:59:49 +0100

    workaround for a pypy jit limitation: this fixes a 10x slowdown which was caused by using chr(ifmt) instead of the old fmt

--- capnpy/unpack.py
+++ capnpy/unpack.py
@@ -1,6 +1,27 @@
 import struct
+from pypytools import IS_PYPY
+
+if IS_PYPY:
+    # workaround for a limitation of the PyPy JIT: struct.unpack is optimized
+    # only if the format string is a tracing-time constant; this is because of
+    # this line in rlib/rstruct/formatiterator.py:
+    #    @jit.look_inside_iff(lambda self, fmt: jit.isconstant(fmt))
+    #    def interpret(self, fmt):
+    #        ...
+    #
+    # The problem is that if you use struct.unpack(chr(113), '...'), chr(113)
+    # is not a tracing-time constant (it becomes constant later, during
+    # optimizeopt). The work around is to use mychr, which pyjitpl.py is smart
+    # enough to detect as a tracing-time constant.
+    _CHR = tuple(map(chr, range(256)))
+    def mychr(i):
+        return _CHR[i]
+
+else:
+    mychr = chr
+
 def unpack_primitive(ifmt, buf, offset):
-    fmt = '<' + chr(ifmt)
+    fmt = '<' + mychr(ifmt)
     if offset < 0 or offset + struct.calcsize(fmt) > len(buf):
         raise IndexError('Offset out of bounds: %d' % offset)
     return struct.unpack_from(fmt, buf, offset)[0]
