574d42f02905fc8a08e3a6a795d456728d96e8e8
Antonio Cuni <anto.cuni@gmail.com>
2016-02-14 03:59:03 +0100

    very rough tool to statically compile a capnp file; doesn't full work yet

--- capnpy/__main__.py
+++ capnpy/__main__.py
@@ -1,11 +1,11 @@
 import sys
+import py
 import time
 from capnpy.message import load
-from capnpy.compiler import load_schema
+from capnpy.compiler import load_schema, Compiler
 
-def main():
-    cmd = sys.argv[1]
-    assert cmd == 'decode'
+
+def decode():
     filename = sys.argv[2]
     schemaname = sys.argv[3]
     clsname = sys.argv[4]
@@ -29,6 +29,39 @@ def main():
                 print >> sys.stderr, i
     c = time.time()
     print >> sys.stderr, 'stream decoded in %.2f secs' % (c-b)
+
+def compile():
+    srcfile = sys.argv[2]
+    if '--pyx' in sys.argv:
+        pyx = True
+    elif '--pyx=no' in sys.argv:
+        pyx = False
+    else:
+        pyx = 'auto'
+    #
+    convert_case = True
+    if '--convert-case=no' in sys.argv:
+        convert_case = False
+    #
+    srcfile = py.path.local(srcfile)
+    dstfile = srcfile.new(ext='.py')
+    comp = Compiler(sys.path, pyx=pyx)
+    m, src = comp.generate_py_source(srcfile, convert_case=convert_case)
+    dstfile.write(src)
+
+    ## mod = load_schema(filename=filename)
+    ## compiled = py.path.local(mod.__file__)
+    ## basedir = py.path.local(filename).dirpath()
+    ## compiled.copy(basedir, mode=True)
+
+def main():
+    cmd = sys.argv[1]
+    if cmd == 'decode':
+        decode()
+    elif cmd == 'compile':
+        compile()
+    else:
+        print 'usage: python -m capnpy decode|compile [ARGS]'
 
 if __name__ == '__main__':
     main()
