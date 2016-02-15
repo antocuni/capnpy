be584d5acd125da6388067102602ab859d5d76b4
Antonio Cuni <anto.cuni@gmail.com>
2016-02-15 14:57:57 +0100

    move ModuleGenerator into its own file

--- capnpy/compiler/__init__.py
+++ capnpy/compiler/__init__.py
@@ -1,117 +1,12 @@
 import py
 import sys
 import types
-from collections import defaultdict
 import subprocess
-import keyword
-from pypytools.codegen import Code
-from capnpy.convert_case import from_camel_case
 from capnpy import schema
 from capnpy.message import loads
 from capnpy.blob import PYX
+from capnpy.compiler.module import ModuleGenerator
 
-# the following imports have side-effects, and augment the schema.* classes
-# with emit() methods
-import capnpy.compiler.request
-import capnpy.compiler.node
-import capnpy.compiler.struct_
-import capnpy.compiler.field
-import capnpy.compiler.misc
-
-
-## # pycapnp will be supported only until the boostrap is completed
-## USE_PYCAPNP = False
-
-## if USE_PYCAPNP:
-##     import capnp
-##     import schema_capnp
-##     def loads(buf, payload_type):
-##         return payload_type.from_bytes(buf)
-## else:
-
-
-class ModuleGenerator(object):
-
-    def __init__(self, request, convert_case, pyx):
-        self.code = Code(pyx=pyx)
-        self.request = request
-        self.convert_case = convert_case
-        self.pyx = pyx
-        self.allnodes = {} # id -> node
-        self.children = defaultdict(list) # nodeId -> nested nodes
-        self.importnames = {} # filename -> import name
- 
-    def w(self, *args, **kwargs):
-        self.code.w(*args, **kwargs)
-
-    def block(self, *args, **kwargs):
-        return self.code.block(*args, **kwargs)
-
-    def register_import(self, fname):
-        name = py.path.local(fname).purebasename
-        name = name.replace('+', 'PLUS')
-        name = '_%s_capnp' % name
-        if name in self.importnames.values():
-            # avoid name clashes
-            name = '%s_%s' % (name, len(self.filenames))
-        self.importnames[fname] = name
-        return name
-
-    def generate(self):
-        self.request.emit(self)
-        return self.code.build()
-
-    def _dump_node(self, node):
-        def visit(node, deep=0):
-            print '%s%s: %s' % (' ' * deep, node.which(), node.displayName)
-            for child in self.children[node.id]:
-                visit(child, deep+2)
-        visit(node)
-
-    def _convert_name(self, name):
-        if self.convert_case:
-            return from_camel_case(name)
-        else:
-            return name
-
-    def _field_name(self, field):
-        name = self._convert_name(field.name)
-        name = self._mangle_name(name)
-        return name
-
-    def _mangle_name(self, name):
-        if name in keyword.kwlist:
-            return name + '_'
-        return name
-
-    def declare_enum(self, var_name, enum_name, items):
-        # this method cannot go on Node__Enum because it's also called by
-        # Node__Struct (for __tag__)
-        items = map(repr, items)
-        decl = "%s = _enum(%r, [%s])" % (var_name, enum_name, ', '.join(items))
-        self.w(decl)
-
-    def def_property(self, ns, name, src):
-        if self.pyx:
-            with ns.block('property {name}:', name=name):
-                with ns.block('def __get__(self):'):
-                    ns.ww(src)
-        else:
-            ns.w('@property')
-            with ns.block('def {name}(self):', name=name):
-                ns.ww(src)
-        ns.w()
-
-    def robust_arglist(self, argnames):
-        # in pyx mode, we cannot use e.g. 'void' as argname: we need to use
-        # 'object void', else cython complains.
-        if self.pyx:
-            def addtype(name):
-                if name.startswith('*'):
-                    return name
-                return 'object %s' % name
-            argnames = [addtype(name) for name in argnames]
-        return argnames
 
 class Compiler(object):
 
---
+++ capnpy/compiler/module.py
@@ -0,0 +1,96 @@
+import py
+import keyword
+from collections import defaultdict
+from pypytools.codegen import Code
+from capnpy.convert_case import from_camel_case
+
+# the following imports have side-effects, and augment the schema.* classes
+# with emit() methods
+import capnpy.compiler.request
+import capnpy.compiler.node
+import capnpy.compiler.struct_
+import capnpy.compiler.field
+import capnpy.compiler.misc
+
+class ModuleGenerator(object):
+
+    def __init__(self, request, convert_case, pyx):
+        self.code = Code(pyx=pyx)
+        self.request = request
+        self.convert_case = convert_case
+        self.pyx = pyx
+        self.allnodes = {} # id -> node
+        self.children = defaultdict(list) # nodeId -> nested nodes
+        self.importnames = {} # filename -> import name
+ 
+    def w(self, *args, **kwargs):
+        self.code.w(*args, **kwargs)
+
+    def block(self, *args, **kwargs):
+        return self.code.block(*args, **kwargs)
+
+    def register_import(self, fname):
+        name = py.path.local(fname).purebasename
+        name = name.replace('+', 'PLUS')
+        name = '_%s_capnp' % name
+        if name in self.importnames.values():
+            # avoid name clashes
+            name = '%s_%s' % (name, len(self.filenames))
+        self.importnames[fname] = name
+        return name
+
+    def generate(self):
+        self.request.emit(self)
+        return self.code.build()
+
+    def _dump_node(self, node):
+        def visit(node, deep=0):
+            print '%s%s: %s' % (' ' * deep, node.which(), node.displayName)
+            for child in self.children[node.id]:
+                visit(child, deep+2)
+        visit(node)
+
+    def _convert_name(self, name):
+        if self.convert_case:
+            return from_camel_case(name)
+        else:
+            return name
+
+    def _field_name(self, field):
+        name = self._convert_name(field.name)
+        name = self._mangle_name(name)
+        return name
+
+    def _mangle_name(self, name):
+        if name in keyword.kwlist:
+            return name + '_'
+        return name
+
+    def declare_enum(self, var_name, enum_name, items):
+        # this method cannot go on Node__Enum because it's also called by
+        # Node__Struct (for __tag__)
+        items = map(repr, items)
+        decl = "%s = _enum(%r, [%s])" % (var_name, enum_name, ', '.join(items))
+        self.w(decl)
+
+    def def_property(self, ns, name, src):
+        if self.pyx:
+            with ns.block('property {name}:', name=name):
+                with ns.block('def __get__(self):'):
+                    ns.ww(src)
+        else:
+            ns.w('@property')
+            with ns.block('def {name}(self):', name=name):
+                ns.ww(src)
+        ns.w()
+
+    def robust_arglist(self, argnames):
+        # in pyx mode, we cannot use e.g. 'void' as argname: we need to use
+        # 'object void', else cython complains.
+        if self.pyx:
+            def addtype(name):
+                if name.startswith('*'):
+                    return name
+                return 'object %s' % name
+            argnames = [addtype(name) for name in argnames]
+        return argnames
