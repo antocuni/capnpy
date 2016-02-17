0eeceb1c468a0b02f54197d6c9e8d448ff44072e
Antonio Cuni <anto.cuni@gmail.com>
2016-02-17 12:05:14 +0000

    automatically build cython if we are on cpython

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+import sys
 import os
 from setuptools import setup, find_packages, Extension
 
-USE_CYTHON = int(os.environ.get('USE_CYTHON', '0'))
+USE_CYTHON = os.environ.get('USE_CYTHON', 'auto')
+if USE_CYTHON == 'auto':
+    is_pypy = hasattr(sys, 'pypy_version_info')
+    USE_CYTHON = not is_pypy
+else:
+    USE_CYTHON = int(USE_CYTHON)
+
 
 if USE_CYTHON:
     from Cython.Build import cythonize
