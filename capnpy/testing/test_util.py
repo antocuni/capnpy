8f24f38aa97493c1043d77dcac742ec6b51646ce
Antonio Cuni <anto.cuni@gmail.com>
2016-02-17 15:02:25 +0000

    write some tests for extend_module_maybe

--- capnpy/testing/test_util.py
+++ capnpy/testing/test_util.py
@@ -1,4 +1,6 @@
-from capnpy.util import extend
+import py
+import textwrap
+from capnpy.util import extend, extend_module_maybe
 
 def test_extend():
     class Foo(object):
@@ -13,3 +15,39 @@ def test_extend():
     assert Bar is Foo
     assert Foo.a == 42
     assert Foo().foo() == 123
+
+
+@py.test.mark.usefixtures('init')
+class TestExtendModuleMaybe(object):
+
+    @py.test.fixture
+    def init(self, tmpdir):
+        self.tmpdir = tmpdir
+
+    def w(self, filename, src):
+        src = textwrap.dedent(src)
+        self.tmpdir.join(filename).write(src)
+
+    def test_filename_simple(self):
+        self.w("foo_extended.py", """
+            answer = 42
+        """)
+        #
+        myglobals = {}
+        extend_module_maybe(myglobals, filename=self.tmpdir.join('foo.py'))
+        assert myglobals['answer'] == 42
+
+    def test_filename_dont_exist(self):
+        myglobals = {}
+        extend_module_maybe(myglobals, filename=self.tmpdir.join('foo.py'))
+        assert myglobals == {}
+
+    def test_modname_simple(self, monkeypatch):
+        monkeypatch.syspath_prepend(self.tmpdir)
+        self.w("foo_extended.py", """
+            answer = 42
+        """)
+        #
+        myglobals = {}
+        extend_module_maybe(myglobals, modname='foo')
+        assert myglobals['answer'] == 42
