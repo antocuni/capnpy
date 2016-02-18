97a27a03d6926f3817a85bdcdd5c09a1053361f2
Antonio Cuni <anto.cuni@gmail.com>
2016-02-18 11:48:17 +0000

    improve the handling of Q: now we build a python long only if it's strictly necessary

--- capnpy/unpack.pyx
+++ capnpy/unpack.pyx
@@ -1,5 +1,5 @@
 from libc.stdint cimport (int8_t, uint8_t, int16_t, uint16_t,
-                          uint32_t, int32_t, int64_t, uint64_t)
+                          uint32_t, int32_t, int64_t, uint64_t, INT64_MAX)
 
 cdef checkbound(int size, bytes buf, int offset):
     if offset + size > len(buf):
@@ -8,7 +8,7 @@ cdef checkbound(int size, bytes buf, int offset):
 cpdef unpack_primitive(char ifmt, bytes buf, int offset):
     cdef char* cbuf
     cdef void* valueaddr
-    cdef long value
+    cdef uint64_t uint64_value
     #
     if offset < 0:
         raise IndexError('Offset out of bounds: %d' % offset)
@@ -18,10 +18,14 @@ cpdef unpack_primitive(char ifmt, bytes buf, int offset):
         checkbound(8, buf, offset)
         return (<int64_t*>valueaddr)[0]
     elif ifmt == 'Q':
+        # if the value is small enough, it returns a python int. Else, a
+        # python long
         checkbound(8, buf, offset)
-        # XXX: this return a Python long: fix it to return a long only if
-        # strictly necessary
-        return (<uint64_t*>valueaddr)[0]
+        uint64_value = (<uint64_t*>valueaddr)[0]
+        if uint64_value <= INT64_MAX:
+            return <int64_t>uint64_value
+        else:
+            return uint64_value
     elif ifmt == 'd':
         checkbound(8, buf, offset)
         return (<double*>valueaddr)[0]
