0d15202b0f78a500fbaed8b4e4147d2b4a7b5ef3
Antonio Cuni <anto.cuni@gmail.com>
2016-02-18 11:30:42 +0000

    implement unpack.pyx for all the supported types

--- capnpy/unpack.pyx
+++ capnpy/unpack.pyx
@@ -1,4 +1,5 @@
-import struct
+from libc.stdint cimport (int8_t, uint8_t, int16_t, uint16_t,
+                          uint32_t, int32_t, int64_t, uint64_t)
 
 cdef checkbound(int size, bytes buf, int offset):
     if offset + size > len(buf):
@@ -15,15 +16,37 @@ cpdef unpack_primitive(char ifmt, bytes buf, int offset):
     valueaddr = cbuf + offset
     if ifmt == 'q':
         checkbound(8, buf, offset)
-        return (<long*>valueaddr)[0]
+        return (<int64_t*>valueaddr)[0]
     elif ifmt == 'Q':
         checkbound(8, buf, offset)
         # XXX: this return a Python long: fix it to return a long only if
         # strictly necessary
-        return (<unsigned long*>valueaddr)[0]
+        return (<uint64_t*>valueaddr)[0]
+    elif ifmt == 'd':
+        checkbound(8, buf, offset)
+        return (<double*>valueaddr)[0]
+    elif ifmt == 'f':
+        checkbound(8, buf, offset)
+        return (<float*>valueaddr)[0]
+    elif ifmt == 'i':
+        checkbound(4, buf, offset)
+        return (<int32_t*>valueaddr)[0]
+    elif ifmt == 'I':
+        checkbound(4, buf, offset)
+        return (<uint32_t*>valueaddr)[0]
+    elif ifmt == 'h':
+        checkbound(2, buf, offset)
+        return (<int16_t*>valueaddr)[0]
+    elif ifmt == 'H':
+        checkbound(2, buf, offset)
+        return (<uint16_t*>valueaddr)[0]
+    elif ifmt == 'b':
+        checkbound(1, buf, offset)
+        return (<int8_t*>valueaddr)[0]
+    elif ifmt == 'B':
+        checkbound(1, buf, offset)
+        return (<uint8_t*>valueaddr)[0]
     #
-    # slow fallback
-    return struct.unpack_from('<' + chr(ifmt), buf, offset)[0]
-    #raise ValueError('unknown fmt %s' % chr(ifmt))
+    raise ValueError('unknown fmt %s' % chr(ifmt))
 
 
