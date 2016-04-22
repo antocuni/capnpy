7d362e9ec02ab3983ad2cccf9ff2f11ff25e1e89
Antonio Cuni <anto.cuni@gmail.com>
2016-04-22 17:24:30 +0200

    forgot to commit this, which is the compiled version of annotate.capnp

---
+++ capnpy/annotate.py
@@ -0,0 +1,40 @@
+# THIS FILE HAS BEEN GENERATED AUTOMATICALLY BY capnpy
+# do not edit by hand
+# generated on 2016-04-22 17:05
+
+from capnpy.struct_ import Struct as _Struct
+from capnpy.struct_ import assert_undefined as _assert_undefined
+from capnpy.struct_ import undefined as _undefined
+from capnpy.enum import enum as _enum
+from capnpy.blob import Types as _Types
+from capnpy.builder import StructBuilder as _StructBuilder
+from capnpy.list import PrimitiveList as _PrimitiveList
+from capnpy.list import StructList as _StructList
+from capnpy.list import StringList as _StringList
+from capnpy.util import text_repr as _text_repr
+from capnpy.util import float32_repr as _float32_repr
+from capnpy.util import float64_repr as _float64_repr
+from capnpy.util import extend_module_maybe as _extend_module_maybe
+
+#### FORWARD DECLARATIONS ####
+
+class nullable(object):
+    __id__ = 11296117080722892765
+    targets_file = False
+    targets_const = False
+    targets_enum = False
+    targets_enumerant = False
+    targets_struct = False
+    targets_field = False
+    targets_union = False
+    targets_group = True
+    targets_interface = False
+    targets_method = False
+    targets_param = False
+    targets_annotation = False
+
+#### DEFINITIONS ####
+
+
+
+_extend_module_maybe(globals(), modname=__name__)
