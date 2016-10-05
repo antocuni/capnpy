e3a1a4e09f2c3cbc2a02ba30c64144f8af88f070
Antonio Cuni <anto.cuni@gmail.com>
2016-10-05 15:11:30 +0200

    fix another test

--- capnpy/testing/compiler/test_repr.py
+++ capnpy/testing/compiler/test_repr.py
@@ -218,7 +218,7 @@ class TestShortRepr(CompilerTest):
         p = self.mod.P.new_x(1)
         self.check(p, '(x = 1)')
         #
-        p = self.mod.P.new_y(None)
+        p = self.mod.P.new_y()
         self.check(p, '(y = void)')
         #
         p = self.mod.P.new_z('hello')
