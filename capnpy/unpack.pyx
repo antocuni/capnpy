5f8db24a02f6a495aab324547fc28c959d3e49ef
Antonio Cuni <anto.cuni@gmail.com>
2016-12-13 17:29:05 +0100

    use PyString_AS_STRING when possible: this seems to give a ~5-10% speedup on reading fields

--- capnpy/unpack.pyx
+++ capnpy/unpack.pyx
@@ -1,9 +1,24 @@
 from libc.stdint cimport (int8_t, uint8_t, int16_t, uint16_t,
                           uint32_t, int32_t, int64_t, uint64_t, INT64_MAX)
+from cpython.string cimport PyString_GET_SIZE, PyString_AS_STRING, PyString_Check
 
+cdef char* as_cbuf(object buf):
+    # PyString_AS_STRING seems to be faster than relying of cython's own logic
+    # to convert bytes to char*
+    cdef bytes bytes_buf
+    if PyString_Check(buf):
+        bytes_buf = buf
+        return PyString_AS_STRING(bytes_buf)
+    else:
+        return buf
 
 cdef checkbound(int size, object buf, int offset):
-    if offset + size > len(buf):
+    cdef Py_ssize_t length = 0
+    if PyString_Check(buf):
+        length = PyString_GET_SIZE(buf)
+    else:
+        length = len(buf)
+    if offset + size > length:
         raise IndexError('Offset out of bounds: %d' % offset)
 
 cpdef unpack_primitive(char ifmt, object buf, int offset):
@@ -13,7 +28,7 @@ cpdef unpack_primitive(char ifmt, object buf, int offset):
     #
     if offset < 0:
         raise IndexError('Offset out of bounds: %d' % offset)
-    cbuf = buf
+    cbuf = as_cbuf(buf)
     valueaddr = cbuf + offset
     if ifmt == 'q':
         checkbound(8, buf, offset)
@@ -61,7 +76,7 @@ cpdef long unpack_int64(object buf, int offset):
     #
     if offset < 0:
         raise IndexError('Offset out of bounds: %d' % offset)
-    cbuf = buf
+    cbuf = as_cbuf(buf)
     valueaddr = cbuf + offset
     checkbound(8, buf, offset)
     return (<int64_t*>valueaddr)[0]
@@ -72,7 +87,7 @@ cpdef long unpack_int16(object buf, int offset):
     #
     if offset < 0:
         raise IndexError('Offset out of bounds: %d' % offset)
-    cbuf = buf
+    cbuf = as_cbuf(buf)
     valueaddr = cbuf + offset
     checkbound(2, buf, offset)
     return (<int16_t*>valueaddr)[0]
@@ -83,7 +98,7 @@ cpdef long unpack_uint32(object buf, int offset):
     #
     if offset < 0:
         raise IndexError('Offset out of bounds: %d' % offset)
-    cbuf = buf
+    cbuf = as_cbuf(buf)
     valueaddr = cbuf + offset
     checkbound(4, buf, offset)
     return (<uint32_t*>valueaddr)[0]
