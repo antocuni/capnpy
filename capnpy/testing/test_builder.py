5451c59250b6576bcc6ed2c1339a85c8f177ac2d
Antonio Cuni <anto.cuni@gmail.com>
2016-12-28 16:37:23 +0100

    start to convert test_builder from StructBuilder to Builder

--- capnpy/testing/test_builder.py
+++ capnpy/testing/test_builder.py
@@ -6,8 +6,11 @@ from capnpy.struct_ import Struct
 from capnpy.printer import print_buffer
 
 def test_primitive():
-    builder = StructBuilder('qqd')
-    buf = builder.build(1, 2, 1.234)
+    builder = Builder(3, 0)
+    builder.set(Types.int64.ifmt, 0, 1)
+    builder.set(Types.int64.ifmt, 8, 2)
+    builder.set(Types.float64.ifmt, 16, 1.234)
+    buf = builder.build()
     assert buf == ('\x01\x00\x00\x00\x00\x00\x00\x00'  # 1
                    '\x02\x00\x00\x00\x00\x00\x00\x00'  # 2
                    '\x58\x39\xb4\xc8\x76\xbe\xf3\x3f') # 1.234
@@ -21,9 +24,9 @@ def test_alloc_struct():
              '\x02\x00\x00\x00\x00\x00\x00\x00')
     mystruct = MyStruct.from_buffer(mybuf, 0, 2, 0)
     #
-    builder = StructBuilder('q')
-    ptr = builder.alloc_struct(0, MyStruct, mystruct)
-    buf = builder.build(ptr)
+    builder = Builder(0, 1)
+    builder.alloc_struct(0, MyStruct, mystruct)
+    buf = builder.build()
     assert buf == ('\x00\x00\x00\x00\x02\x00\x00\x00'  # ptr to mystruct
                    + mybuf)
 
