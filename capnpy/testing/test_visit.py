fc071e271ef5f44caba3c342c58f8c970bcf2b06
Antonio Cuni <anto.cuni@gmail.com>
2017-02-22 11:47:35 +0100

    add two passing tests

--- capnpy/testing/test_visit.py
+++ capnpy/testing/test_visit.py
@@ -320,3 +320,54 @@ class TestIsCompact(object):
                                      size_tag=ptr.LIST_SIZE_COMPOSITE,
                                      item_count=3)
         assert is_compact
+
+    def test_list_composite_not_compact(self):
+        ## struct Point {
+        ##   x @0 :Int64;
+        ##   y @1 :Int64;
+        ##   name @2 :Text;
+        ## }
+        buf = ('garbage0'
+               '\x0c\x00\x00\x00\x02\x00\x01\x00'   # list tag
+               '\x01\x00\x00\x00\x00\x00\x00\x00'   # points[0].x == 1
+               '\x02\x00\x00\x00\x00\x00\x00\x00'   # points[0].y == 2
+               '\x1d\x00\x00\x00\x42\x00\x00\x00'   # points[0].name == ptr
+               '\x03\x00\x00\x00\x00\x00\x00\x00'   # points[1].x == 3
+               '\x04\x00\x00\x00\x00\x00\x00\x00'   # points[1].y == 4
+               '\x15\x00\x00\x00\x42\x00\x00\x00'   # points[1].name == ptr
+               '\x05\x00\x00\x00\x00\x00\x00\x00'   # points[2].x == 5
+               '\x06\x00\x00\x00\x00\x00\x00\x00'   # points[2].y == 6
+               '\x0d\x00\x00\x00\x42\x00\x00\x00'   # points[2].name == ptr
+               'garbage1'
+               'P' 'o' 'i' 'n' 't' ' ' 'A' '\x00'
+               'P' 'o' 'i' 'n' 't' ' ' 'B' '\x00'
+               'P' 'o' 'i' 'n' 't' ' ' 'C' '\x00'
+               'garbage2')
+        is_compact = self.is_compact(buf, 8, ptr.LIST,
+                                     size_tag=ptr.LIST_SIZE_COMPOSITE,
+                                     item_count=3)
+        assert not is_compact
+
+
+    def test_list_composite_nullptr(self):
+        ## struct Point {
+        ##   x @0 :Int64;
+        ##   y @1 :Int64;
+        ##   name @2 :Text;
+        ## }
+        buf = ('garbage0'
+               '\x0c\x00\x00\x00\x02\x00\x01\x00'   # list tag
+               '\x01\x00\x00\x00\x00\x00\x00\x00'   # points[0].x == 1
+               '\x02\x00\x00\x00\x00\x00\x00\x00'   # points[0].y == 2
+               '\x00\x00\x00\x00\x00\x00\x00\x00'   # points[0].name == NULL
+               '\x03\x00\x00\x00\x00\x00\x00\x00'   # points[1].x == 3
+               '\x04\x00\x00\x00\x00\x00\x00\x00'   # points[1].y == 4
+               '\x00\x00\x00\x00\x00\x00\x00\x00'   # points[1].name == NULL
+               '\x05\x00\x00\x00\x00\x00\x00\x00'   # points[2].x == 5
+               '\x06\x00\x00\x00\x00\x00\x00\x00'   # points[2].y == 6
+               '\x00\x00\x00\x00\x00\x00\x00\x00'   # points[2].name == NULL
+               'garbage1')
+        is_compact = self.is_compact(buf, 8, ptr.LIST,
+                                     size_tag=ptr.LIST_SIZE_COMPOSITE,
+                                     item_count=3)
+        assert is_compact
