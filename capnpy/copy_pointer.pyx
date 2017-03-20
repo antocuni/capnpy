6846687ed2aed9f4b4bbd89c2c7f725592664f8c
Antonio Cuni <anto.cuni@gmail.com>
2017-03-20 15:59:33 +0100

    add a nice method to allocate a struct and get a pointer to the area to fill

--- capnpy/copy_pointer.pyx
+++ capnpy/copy_pointer.pyx
@@ -1,4 +1,6 @@
 cimport cython
+from libc.stdint cimport (int8_t, uint8_t, int16_t, uint16_t,
+                          uint32_t, int32_t, int64_t, uint64_t)
 from libc.string cimport memcpy
 from cpython.string cimport PyString_AS_STRING, PyString_FromStringAndSize
 from capnpy cimport ptr
@@ -23,12 +25,31 @@ cdef class MutableBuffer(object):
         self.cbuf = as_cbuf(self.buf, &unused)
         self.end = 0
 
-    cpdef char* allocate(self, long length) except NULL:
-        cdef long end = self.end
+    cpdef as_bytes(self):
+        return PyString_FromStringAndSize(self.cbuf, self.end)
+
+    cpdef void set_int64(self, long i, int64_t value):
+        (<int64_t*>(self.cbuf+i))[0] = value
+
+    cpdef long allocate(self, long length) except -1:
+        """
+        Allocate ``length`` bytes of memory inside the buffer. Return the start
+        position of the newly allocated space.
+        """
+        cdef long result = self.end
         self.end += length
         if self.end > self.length:
             raise ValueError("buffer too small; TODO: implement resizing or multiple segments")
-        return self.cbuf + end
+        return result
 
-    cpdef as_bytes(self):
-        return PyString_FromStringAndSize(self.cbuf, self.end)
+    cpdef long new_struct(self, long i, long data_size, long ptrs_size) except -1:
+        """
+        Allocate a new struct of the given size, and write the resulting pointer
+        at position i. Return the newly allocated position.
+        """
+        cdef long length = (data_size+ptrs_size) * 8
+        cdef long result = self.allocate(length)
+        cdef long offet = result - (i+8)
+        cdef long p = ptr.new_struct(offet/8, data_size, ptrs_size)
+        self.set_int64(i, p)
+        return result
--- capnpy/testing/test_copy_pointer.py
+++ capnpy/testing/test_copy_pointer.py
@@ -1,16 +1,41 @@
 import pytest
+from capnpy.printer import print_buffer
 from capnpy.copy_pointer import MutableBuffer
 
 class TestMutableBuffer(object):
 
     def test_allocate(self):
         buf = MutableBuffer(16)
-        buf.allocate(8)
+        assert buf.allocate(8) == 0
         s = buf.as_bytes()
         assert s == '\x00' * 8
 
     def test_allocate_failure(self):
         buf = MutableBuffer(16)
-        buf.allocate(8)
-        buf.allocate(8)
+        assert buf.allocate(8) == 0
+        assert buf.allocate(8) == 8
         pytest.raises(ValueError, "buf.allocate(1)")
+
+    def test_set_int64(self):
+        buf = MutableBuffer(8)
+        buf.allocate(8)
+        buf.set_int64(0, 0x1234ABCD)
+        s = buf.as_bytes()
+        assert s == '\xCD\xAB\x34\x12\x00\x00\x00\x00'
+
+    def test_new_struct(self):
+        buf = MutableBuffer(64)
+        buf.allocate(16)
+        a = buf.new_struct(0, data_size=3, ptrs_size=0)
+        b = buf.new_struct(8, data_size=1, ptrs_size=0)
+        buf.set_int64(a,    1)
+        buf.set_int64(a+8,  2)
+        buf.set_int64(a+16, 3)
+        buf.set_int64(b,    4)
+        s = buf.as_bytes()
+        assert s == ('\x04\x00\x00\x00\x03\x00\x00\x00'   # ptr to a (3, 0)
+                     '\x0c\x00\x00\x00\x01\x00\x00\x00'   # ptr to b (1, 0)
+                     '\x01\x00\x00\x00\x00\x00\x00\x00'   # a: 1
+                     '\x02\x00\x00\x00\x00\x00\x00\x00'   #    2
+                     '\x03\x00\x00\x00\x00\x00\x00\x00'   #    3
+                     '\x04\x00\x00\x00\x00\x00\x00\x00')  # b: 4
