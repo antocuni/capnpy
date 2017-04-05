antocuni/capnpy

commit f4aa08fdd2c3bd8aa57395b5031f0b019bf8e8e0

comment out @cython.except_ for now, while we wait for my fork to be merged

capnpy/segment/_copy_pointer.py
@@ -45,9 +45,20 @@ if not cython.compiled:
     globals()['check_bounds'] = _check_bounds
     globals()['read_int64_fast'] = _read_int64_fast
 
+
+# depending on the phase of the moon, I saw speeupds up to 20% if we declare
+# all these functions as returning a long and "except -1". However, other
+# times I didn't see any significant speedup.
+#
+# For now, the decorators are commented out because @cython.except_ is
+# available only in my Cython fork:
+#    https://github.com/antocuni/cython/tree/pure-except
+#
+# we can re-enable them as soon as the fork is merged.
+
 @cython.ccall
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(src=BaseSegment, p=long, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                kind=long)
 def copy_pointer(src, p, src_pos, dst, dst_pos):
@@ -72,8 +83,8 @@ def copy_pointer(src, p, src_pos, dst, dst_pos):
 
 
 @cython.cfunc
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(n=long, src=BaseSegment, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                i=long, p=long, offset=long)
 def _copy_many_ptrs(n, src, src_pos, dst, dst_pos):
@@ -86,8 +97,8 @@ def _copy_many_ptrs(n, src, src_pos, dst, dst_pos):
 
 
 @cython.cfunc
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(src=BaseSegment, p=long, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                data_size=long, ptrs_size=long, ds=long)
 def _copy_struct(src, p, src_pos, dst, dst_pos):
@@ -102,8 +113,8 @@ def _copy_struct(src, p, src_pos, dst, dst_pos):
 
 
 @cython.cfunc
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(src=BaseSegment, p=long, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                count=long, size_tag=long, body_length=long)
 def _copy_list_primitive(src, p, src_pos, dst, dst_pos):
@@ -122,8 +133,8 @@ def _copy_list_primitive(src, p, src_pos, dst, dst_pos):
 
 
 @cython.cfunc
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(src=BaseSegment, p=long, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                count=long, body_length=long)
 def _copy_list_ptr(src, p, src_pos, dst, dst_pos):
@@ -136,8 +147,8 @@ def _copy_list_ptr(src, p, src_pos, dst, dst_pos):
 
 
 @cython.cfunc
-@cython.returns(long)
-@cython.except_(-1)
+##@cython.returns(long)
+##@cython.except_(-1)
 @cython.locals(src=BaseSegment, p=long, src_pos=long, dst=SegmentBuilder, dst_pos=long,
                total_words=long, body_length=long,
                tag=long, count=long, data_size=long, ptrs_size=long,

commit 1bea1f4c23dcdbccf78f510e79dfa7cfbbc17eb4

fix the signature of check_bounds in copy_pointer: this fixes the segfaults introduced in f4aa08fdd2c3bd8aa57395b5031f0b019bf8e8e0

capnpy/segment/_copy_pointer.h
@@ -1,9 +1,9 @@
 // XXX: how to avoid to hard-copy the C name generated for raise_out_of_bound?
 #define RAISE_OUT_OF_BOUNDS __pyx_f_6capnpy_7segment_7builder_raise_out_of_bounds
-static long RAISE_OUT_OF_BOUNDS(Py_ssize_t, Py_ssize_t);
+static PyObject* RAISE_OUT_OF_BOUNDS(Py_ssize_t, Py_ssize_t);
 
 #define CHECK_BOUNDS(src, size, offset)                                 \
-    0; /* return non-error value by default */                          \
+    (Py_INCREF(Py_None), Py_None);                                      \
     {                                                                   \
         if ((offset)+(size) > (PyString_GET_SIZE(src->buf))) {          \
             /* raise and return error */                                \

capnpy/segment/_copy_pointer.py
@@ -54,7 +54,9 @@ if not cython.compiled:
 # available only in my Cython fork:
 #    https://github.com/antocuni/cython/tree/pure-except
 #
-# we can re-enable them as soon as the fork is merged.
+# we can re-enable them as soon as the fork is merged. If we do, remember to
+# change the signature of check_bounds and raise_out_of_bounds in
+# _copy_pointer.h and _copy_pointer.pyx accordingly.
 
 @cython.ccall
 ##@cython.returns(long)

capnpy/segment/_copy_pointer.pyx
@@ -26,9 +26,9 @@ cdef int64_t read_int64_fast(BaseSegment src, long i):
 # with a fast-path (the bound check), and we moved the slow path inside the
 # raise_out_of_bound function. This seems to give a ~40% speedup!
 cdef extern from "_copy_pointer.h":
-    long check_bounds "CHECK_BOUNDS" (BaseSegment src, Py_ssize_t size, Py_ssize_t offset) except -1
+    object check_bounds "CHECK_BOUNDS" (BaseSegment src, Py_ssize_t size, Py_ssize_t offset)
 
-cdef long raise_out_of_bounds(Py_ssize_t size, Py_ssize_t offset) except -1:
+cdef object raise_out_of_bounds(Py_ssize_t size, Py_ssize_t offset):
     raise IndexError('Offset out of bounds: %d' % (offset+size))
- 
+
 # ======================