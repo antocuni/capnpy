278b03bf15302912cadc1dabc9815f539dbfd8e3
Antonio Cuni <anto.cuni@gmail.com>
2017-04-25 15:21:45 +0200

    kill _read_enum also from tests

--- capnpy/testing/test_enum.py
+++ capnpy/testing/test_enum.py
@@ -14,19 +14,6 @@ def test_enum():
     assert Color(0) == Color.red
     py.test.raises(AttributeError, "Color.red.x = 42")
 
-def test_read_enum():
-    Color = enum('Color', ('red', 'green', 'blue', 'yellow'))
-    Gender = enum('Gender', ('male', 'female', 'unknown'))
-    #      color      gender     padding
-    buf = '\x02\x00' '\x01\x00' '\x00\x00\x00\x00'
-    
-    blob = Struct.from_buffer(buf, 0, data_size=1, ptrs_size=0)
-    color = blob._read_enum(0, Color)
-    gender = blob._read_enum(2, Gender)
-    assert color == Color.blue
-    assert gender == Gender.female
-
-
 def test_unknown():
     Color = enum('Color', ('red', 'green', 'blue'))
     assert type(Color) is type
