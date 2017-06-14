ad30282a0b07ed06c5268be3198de6521eb74dc4
Antonio Cuni <anto.cuni@gmail.com>
2017-06-14 16:45:42 +0200

    add capnpy.__version__

--- capnpy/__init__.py
+++ capnpy/__init__.py
@@ -1,7 +1,13 @@
 import sys
+import pkg_resources
 from capnpy.compiler.compiler import DynamicCompiler
 from capnpy.compiler.distutils import capnpify
 from capnpy.message import load, loads, load_all, dumps, dump
+
+try:
+    __version__ = pkg_resources.get_distribution('capnpy').version
+except Exception:
+    __version__ = 'unknown'
 
 _compiler = DynamicCompiler(sys.path)
 load_schema = _compiler.load_schema
