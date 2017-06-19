b8061af9cf942bc2fc739fe93d630e3bd00ff500
Antonio Cuni <anto.cuni@gmail.com>
2017-06-19 12:13:46 +0200

    automatically build the linux wheels if the tests pass

---
+++ travis/build-wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/cp27-*/bin; do
+    "${PYBIN}/pip" install 'cython>=0.25'
+    "${PYBIN}/pip" wheel /capnpy/ -w wheelhouse/
+
+    # create the sdist if it does not exist yet
+    if [[ ! -d /capnpy/dist ]]
+    then
+        (cd /capnpy && "${PYBIN}/python" setup.py sdist)
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /capnpy/wheelhouse/
+done
