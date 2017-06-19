03ae5847c13e75d744ec928fd21fcd4032f40a1a
Antonio Cuni <anto.cuni@gmail.com>
2017-06-19 16:16:36 +0200

    use an even better approach to handle Cython transparently, by overriding some distutils commands

--- setup.py
+++ setup.py
@@ -1,34 +1,119 @@
 import sys
 import os
 from setuptools import setup, find_packages, Extension
+from distutils.command.build_ext import build_ext
+from distutils.command.sdist import sdist
 
-try:
-    import Cython
-    if Cython.__version__ < '0.25':
-        print ('WARNING: required cython>0.25, found %s. The .c files will '
-               'NOT be regenerated' % Cython.__version__)
-        raise ImportError
-    from Cython.Build import cythonize
-except ImportError:
-    def cythonize(extensions, **kwargs):
-        # dummy version of cythonize, for when Cython is not installed. This
-        # works only if you install from an sdist package, which contains the
-        # already-converted C files
-        def cname(fname):
-            fname = fname.replace('.pyx', '.c').replace('.py', '.c')
-            if not os.path.exists(fname):
+"""
+In the following, we speak about Cython being required or not: we are
+talking about "setup.py-time requirements", i.e. if Cython needs to be
+installed **before** trying to install capnpy.  Once the setup.py is running,
+Cython will always be installed transparently because it's listed in
+`install_requires`.
+
+The following applies only to CPython; Cython is never required on PyPy.
+
+There are various use-cases to consider:
+
+  1. manual `setup.py sdist`: the custom command my_sdist make sure to
+     generate the C files and include them in the .tar.gz. Cython is REQUIRED.
+
+  2. manual install from the sdist tarball: it will use already-generated C
+     files. Cython is NOT required.
+
+  3. manual install from a git checkout: the C files will NOT be there; the
+     custom command my_build_ext will take care of generate them
+     automatically. Cython is REQUIRED.
+
+  4. 'pip install capnpy': in most cases it will download a prebuilt binary
+     wheel from pypi; in the others, it will build from the sdist. In both
+     cases, Cython is NOT required.
+
+  5. 'pip install git+http://github.com/...': pip will first install Cython
+     (because it's in install_requires), then install capnpy (and since Cython
+     is installed, the C files will be automatically generated). Thus, Cython
+     is handled completely transparently.
+
+So, Cython is handled transparently in cases (4) and (5), which are the two
+most common ones.
+
+In theory, we could add 'cython>=0.25' to setup_requires. This way, Cython
+will be handled transparently also in the remaining cases. However:
+
+  - if we do this setuptools tries to install cython in *all* cases
+
+  - this installation is done by setuptools/easy_install, NOT pip
+
+  - setuptools does not support installing from wheels; the result is that it
+    tries to compile Cython, which is slow
+
+  - this make cases (4) and (5) considerably slower than needed, because pip
+    will be forced to wait for setuptools to compile cython, instead of doing
+    a fast install from a wheel.
+
+So, we chose to speep up the common cases, at the cost of requiring an
+explicit installation of Cython in the non common cases (1) and (3).
+"""
+
+
+###############################################################################
+# Custom distutils commands
+
+DEBUG = False # whether to compile files with -g
+
+def my_cythonize(extensions):
+    try:
+        import Cython
+        if Cython.__version__ < '0.25':
+            print ('WARNING: required cython>0.25, found %s. The .c files will '
+                   'NOT be regenerated' % Cython.__version__)
+            raise ImportError
+        from Cython.Build import cythonize
+    except ImportError:
+        return cythonize_dummy(extensions)
+    else:
+        return cythonize(extensions, gdb_debug=DEBUG)
+
+def cythonize_dummy(extensions):
+    def cname(fname):
+        return fname.replace('.pyx', '.c').replace('.py', '.c')
+    #
+    for ext in extensions:
+        ext.sources = [cname(s) for s in ext.sources]
+        for src in ext.sources:
+            if not os.path.exists(src):
                 print ('%s does not exist and Cython is not installed. '
                        'Please install Cython to regenerate it '
-                       'automatically.' % fname)
+                       'automatically.' % src)
                 sys.exit(1)
-            return fname
-        #
-        for ext in extensions:
-            ext.sources = [cname(s) for s in ext.sources]
-        return extensions
+    return extensions
+
+
+class my_sdist(sdist):
+    """
+    Same as the standard sdist, but make sure to cythonize the files first.
+    """
+
+    def run(self):
+        my_cythonize(self.distribution.ext_modules)
+        return sdist.run(self)
+
+
+class my_build_ext(build_ext):
+    """
+    Same as the standard build_ext, but tries to handle cythonize as much
+    transparently as possible. In particular, if Cython is not installed, it
+    tries to reuse the existing *.c files which were included in the sdist.
+    """
+    def build_extensions(self):
+        self.extensions = my_cythonize(self.extensions)
+        return build_ext.build_extensions(self)
+
+# end of the custom commands section
+###############################################################################
+
 
 def get_cython_extensions():
-    DEBUG = False # whether to compile files with -g
     files = ["capnpy/segment/base.pyx",
              "capnpy/segment/segment.py",
              "capnpy/segment/builder.pyx",
@@ -62,7 +147,8 @@ def get_cython_extensions():
             include_dirs = [capnpy_dir],
             extra_compile_args = extra_compile_args,
         )
-    return cythonize(map(getext, files), gdb_debug=DEBUG)
+    return [getext(f) for f in files]
+
 
 if hasattr(sys, 'pypy_version_info'):
     # on PyPy
@@ -79,12 +165,17 @@ else:
     ext_modules = []
     extra_install_requires = []
 
+
 setup(name="capnpy",
       author='Antonio Cuni',
       author_email='anto.cuni@gmail.com',
       url='https://bitbucket.org/antocuni/capnpy',
       use_scm_version=True,
       include_package_data=True,
+      cmdclass={
+          'sdist': my_sdist,
+          'build_ext': my_build_ext,
+      },
       packages = find_packages(),
       ext_modules = ext_modules,
       install_requires=['pypytools>=0.3.2', 'docopt'] + extra_install_requires,
