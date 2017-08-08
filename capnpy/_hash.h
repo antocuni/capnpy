f693b6bdf356d783c324b21fc2c069d485f09f06
Peter Kovary <peterkovary@gambitresearch.com>
2017-08-08 16:04:47 +0100

    Squelch a warning about unsigned long Should be evaluated at compile time anyway

--- capnpy/_hash.h
+++ capnpy/_hash.h
@@ -8,18 +8,14 @@ typedef struct {
     long suffix;
 } _Py_HashSecret_custom;
 
-#define MINLONG -9223372036854775808
+#define MINLONG -9223372036854775807 -1
 
 #if PY_MAJOR_VERSION < 3
-
     #define HASH_MASK 0
     Py_hash_t strhash_f(const void *src, Py_ssize_t len) {return 0;}
     #define HashSecret _Py_HashSecret
-
 #else
-
     #define HASH_MASK _PyHASH_MODULUS
     #define strhash_f _Py_HashBytes
     _Py_HashSecret_custom HashSecret; // uninitialized dummy
-
 #endif
