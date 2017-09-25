fa3d03b36555b70c26aeb6bd56c6a5b527231a39
Antonio Cuni <anto.cuni@gmail.com>
2017-09-25 16:40:59 +0200

    fix py3k

--- capnpy/testing/segment/test_copy_pointer.py
+++ capnpy/testing/segment/test_copy_pointer.py
@@ -131,12 +131,12 @@ class TestCopyPointer(object):
         # test that we copy "empty structs" correctly. These are structs with
         # data_size == ptr_size == 0; in this case, our copy_pointer always
         # generates a ptr whose offset==-1, whatever the orignal offset was.
-        src = (
+        src = b(
             '\x01\x00\x00\x00\x00\x00\x00\x00'    # color == 1
             '\xfc\xff\xBB\xAA\x00\x00\x00\x00'    # empty ptr, arbitrary offset
             '\x00\x00\x00\x00\x00\x00\x00\x00')   # ptr to b, NULL
         dst = self.copy_struct(src, offset=0, data_size=1, ptrs_size=2)
-        assert dst == (
+        assert dst == b(
             '\x00\x00\x00\x00\x01\x00\x02\x00'    # ptr to Rectangle (1, 2)
             '\x01\x00\x00\x00\x00\x00\x00\x00'    # color == 1
             '\xfc\xff\xff\xff\x00\x00\x00\x00'    # empty ptr, offset == -1
