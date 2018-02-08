bc58c974a834b02e3f6bef177572d8709984b7d5
Antonio Cuni <anto.cuni@gmail.com>
2018-02-08 17:28:35 +0100

    kill the generic Visitor, and leave only the logic needed to implement endof

--- capnpy/segment/endof.py
+++ capnpy/segment/endof.py
@@ -1,59 +1,9 @@
 from capnpy import ptr
 
-class Visitor(object):
-    """
-    Generic logic for visiting an arbitrary capnp object.
-    """
-
-    def visit(self, buf, p, offset):
-        kind = ptr.kind(p)
-        offset = ptr.deref(p, offset)
-        if kind == ptr.STRUCT:
-            data_size = ptr.struct_data_size(p)
-            ptrs_size = ptr.struct_ptrs_size(p)
-            return self.visit_struct(buf, p, offset, data_size, ptrs_size)
-        elif kind == ptr.LIST:
-            item_size = ptr.list_size_tag(p)
-            count = ptr.list_item_count(p)
-            if item_size == ptr.LIST_SIZE_COMPOSITE:
-                tag = buf.read_ptr(offset)
-                count = ptr.offset(tag)
-                data_size = ptr.struct_data_size(tag)
-                ptrs_size = ptr.struct_ptrs_size(tag)
-                return self.visit_list_composite(buf, p, offset,
-                                                  count, data_size, ptrs_size)
-            elif item_size == ptr.LIST_SIZE_PTR:
-                return self.visit_list_ptr(buf, p, offset, count)
-            elif item_size == ptr.LIST_SIZE_BIT:
-                return self.visit_list_bit(buf, p, offset, count)
-            else:
-                return self.visit_list_primitive(buf, p, offset, item_size, count)
-        elif kind == ptr.FAR:
-            # this is not strictly correct, because it's part of the logic of
-            # EndOf. However, we are going to refactor this shortly
-            raise NotCompact
-        else:
-            assert False, 'unknown ptr kind'
-
-    def visit_struct(self, buf, p, offset, data_size, ptrs_size):
-        raise NotImplementedError
-
-    def visit_list_composite(self, buf, p, offset, count, data_size, ptrs_size):
-        raise NotImplementedError
-
-    def visit_list_ptr(self, buf, p, offset, count):
-        raise NotImplementedError
-
-    def visit_list_primitive(self, buf, p, offset, item_size, count):
-        raise NotImplementedError
-
-    def visit_list_bit(self, buf, p, offset, count):
-        raise NotImplementedError
-
 class NotCompact(Exception):
     pass
 
-class EndOf(Visitor):
+def endof(seg, p, offset):
     """
     Check whether the given object is compact, and in that case compute its
     end boundary. If it's not compact, return -1.
@@ -68,61 +18,82 @@ class EndOf(Visitor):
 
       4. there are no FAR pointers
     """
-
-    def visit_ptrs(self, buf, offset, ptrs_size, current_end):
-        i = 0
-        while i < ptrs_size:
-            p_offset = offset + i*8
-            i += 1
-            p = buf.read_ptr(p_offset)
-            if not p:
-                continue
-            new_start = ptr.deref(p, p_offset)
-            if new_start != current_end:
-                raise NotCompact
-            current_end = self.visit(buf, p, p_offset)
-        #
-        return current_end
-
-    def visit_struct(self, buf, p, offset, data_size, ptrs_size):
-        offset += data_size*8
-        current_end = offset + (ptrs_size*8)
-        return self.visit_ptrs(buf, offset, ptrs_size, current_end)
-
-    def visit_list_composite(self, buf, p, offset, count, data_size, ptrs_size):
-        item_size = (data_size+ptrs_size)*8
-        offset += 8 # skip the tag
-        end = offset + (item_size)*count
-        if ptrs_size == 0:
-            return end
-        #
-        i = 0
-        while i < count:
-            item_offset = offset + (item_size)*i + (data_size*8)
-            end = self.visit_ptrs(buf, item_offset, ptrs_size, end)
-            i += 1
-        #
-        return end
-
-    def visit_list_ptr(self, buf, p, offset, count):
-        end = offset + 8*count
-        return self.visit_ptrs(buf, offset, count, end)
-
-    def visit_list_primitive(self, buf, p, offset, item_size, count):
-        item_size = ptr.list_item_length(item_size)
-        return ptr.round_up_to_word(offset + item_size*count)
-
-    def visit_list_bit(self, buf, p, offset, count):
-        bytes_length = ptr.round_up_to_word(count) // 8
-        return ptr.round_up_to_word(offset + bytes_length)
-
-
-
-def endof(buf, p, offset):
     try:
-        return _end_of.visit(buf, p, offset)
+        return _endof(seg, p, offset)
     except NotCompact:
         return -1
 
+def _endof(seg, p, offset):
+    kind = ptr.kind(p)
+    offset = ptr.deref(p, offset)
+    if kind == ptr.STRUCT:
+        data_size = ptr.struct_data_size(p)
+        ptrs_size = ptr.struct_ptrs_size(p)
+        return _endof_struct(seg, p, offset, data_size, ptrs_size)
+    elif kind == ptr.LIST:
+        item_size = ptr.list_size_tag(p)
+        count = ptr.list_item_count(p)
+        if item_size == ptr.LIST_SIZE_COMPOSITE:
+            tag = seg.read_ptr(offset)
+            count = ptr.offset(tag)
+            data_size = ptr.struct_data_size(tag)
+            ptrs_size = ptr.struct_ptrs_size(tag)
+            return _endof_list_composite(seg, p, offset,
+                                         count, data_size, ptrs_size)
+        elif item_size == ptr.LIST_SIZE_PTR:
+            return _endof_list_ptr(seg, p, offset, count)
+        elif item_size == ptr.LIST_SIZE_BIT:
+            return _endof_list_bit(seg, p, offset, count)
+        else:
+            return _endof_list_primitive(seg, p, offset, item_size, count)
+    elif kind == ptr.FAR:
+        raise NotCompact
+    else:
+        assert False, 'unknown ptr kind'
 
-_end_of = EndOf()
+def _endof_ptrs(seg, offset, ptrs_size, current_end):
+    i = 0
+    while i < ptrs_size:
+        p_offset = offset + i*8
+        i += 1
+        p = seg.read_ptr(p_offset)
+        if not p:
+            continue
+        new_start = ptr.deref(p, p_offset)
+        if new_start != current_end:
+            raise NotCompact
+        current_end = _endof(seg, p, p_offset)
+    #
+    return current_end
+
+def _endof_struct(seg, p, offset, data_size, ptrs_size):
+    offset += data_size*8
+    current_end = offset + (ptrs_size*8)
+    return _endof_ptrs(seg, offset, ptrs_size, current_end)
+
+def _endof_list_composite(seg, p, offset, count, data_size, ptrs_size):
+    item_size = (data_size+ptrs_size)*8
+    offset += 8 # skip the tag
+    end = offset + (item_size)*count
+    if ptrs_size == 0:
+        return end
+    #
+    i = 0
+    while i < count:
+        item_offset = offset + (item_size)*i + (data_size*8)
+        end = _endof_ptrs(seg, item_offset, ptrs_size, end)
+        i += 1
+    #
+    return end
+
+def _endof_list_ptr(seg, p, offset, count):
+    end = offset + 8*count
+    return _endof_ptrs(seg, offset, count, end)
+
+def _endof_list_primitive(seg, p, offset, item_size, count):
+    item_size = ptr.list_item_length(item_size)
+    return ptr.round_up_to_word(offset + item_size*count)
+
+def _endof_list_bit(seg, p, offset, count):
+    bytes_length = ptr.round_up_to_word(count) // 8
+    return ptr.round_up_to_word(offset + bytes_length)
