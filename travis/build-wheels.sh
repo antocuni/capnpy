6828bee91c2cda07128d5aab4042160233ed9c73
Antonio Cuni <anto.cuni@gmail.com>
2018-10-02 15:13:57 +0000

    build wheels for all supported pythons

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,14 +2,22 @@
 set -e -x
 
 # Compile wheels
-for PYBIN in /opt/python/cp27-*/bin; do
-    "${PYBIN}/pip" install 'cython>=0.25'
-    "${PYBIN}/pip" wheel /capnpy/ -w wheelhouse/
+PYTHONS=(
+    cp27-cp27m
+    cp27-cp27mu
+    cp35-cp35m
+    cp36-cp36m
+    )
+
+for pydir in "${PYTHONS[@]}"; do
+    pybin=/opt/python/$pydir/bin
+    "${pybin}/pip" install 'cython>=0.25'
+    "${pybin}/pip" wheel /capnpy/ -w wheelhouse/
 
     # create the sdist if it does not exist yet
     if [[ ! -d /capnpy/dist ]]
     then
-        (cd /capnpy && "${PYBIN}/python" setup.py sdist)
+        (cd /capnpy && "${pybin}/python" setup.py sdist)
     fi
 done
 
