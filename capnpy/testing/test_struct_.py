b771ab43182cd4037d80ab72f8e3b93b6be2c324
Antonio Cuni <anto.cuni@gmail.com>
2019-05-23 10:29:09 +0200

    py3k fix

--- capnpy/testing/test_struct_.py
+++ capnpy/testing/test_struct_.py
@@ -95,8 +95,8 @@ def test_null_pointers():
     val = b'dummy default value'
     assert blob._read_list(0, None, default_=val) is val
     txt = blob._read_str_text(0, default_=val)
-    assert txt == val
-    assert isinstance(txt, unicode)
+    assert txt == u'dummy default value'
+
 
 def test_far_pointer():
     # see also test_list.test_far_pointer
