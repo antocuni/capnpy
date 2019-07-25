antocuni/capnpy

commit b55d4bc7695838d2b8ff62887f48608c82a92bd4

make it possible to specify defaults for .nullable fields

capnpy/compiler/fieldtree.py
@@ -109,18 +109,26 @@ class Node(AbstractNode):
     def _init_default(self, m):
         # self.default is a *string* containing a Python repr of the default
         # value
-        if self.f.is_part_of_union() and not self.force_default:
-            self.default = '_undefined'
-        elif self.f.is_slot():
-            default_val = self.f.slot.defaultValue.as_pyobj()
-            self.default = str(default_val)
-        else:
-            assert self.f.is_group()
-            if self.f.is_nullable(m):
-                self.default = 'None'
+        def get_default(f):
+            if f.is_part_of_union() and not self.force_default:
+                return '_undefined'
+            elif f.is_slot():
+                default_val = f.slot.defaultValue.as_pyobj()
+                return str(default_val)
+            elif f.is_nullable(m):
+                ann = f.is_nullable(m)
+                name, f_is_null, f_value = ann.check(m)
+                default_is_null = f_is_null.slot.defaultValue.as_pyobj()
+                if default_is_null:
+                    return 'None'
+                else:
+                    return get_default(f_value)
             else:
+                assert f.is_group()
                 items = [child.default for child in self.children]
-                self.default = '(%s,)' % ', '.join(items)
+                return '(%s,)' % ', '.join(items)
+
+        self.default = get_default(self.f)
 
     def __repr__(self):
         return '<Node %s: %s>' % (self.varname, self.f.which())

capnpy/testing/compiler/test_null.py
@@ -83,7 +83,7 @@ class TestNullable(CompilerTest):
         using Py = import "/capnpy/annotate.capnp";
         struct Foo {
             x :group $Py.nullable {
-                isNull @0 :Int8;
+                isNull @0 :Bool;
                 value  @1 :Int64;
             }
         }
@@ -126,9 +126,9 @@ class TestNullable(CompilerTest):
         assert foo.x == 42
         #
         foo = mod.Foo()
-        assert foo._x.is_null
+        assert not foo._x.is_null
         assert foo._x.value == 0
-        assert foo.x is None
+        assert foo.x == 0
 
     def test_ctor_nullable_group(self, mod):
         # check that we can pass a null value
@@ -149,14 +149,13 @@ class TestNullable(CompilerTest):
         assert bar.point.x == 1
         assert bar.point.y == 2
 
-
     def test_bad_nullable(self):
         schema = """
         @0xbf5147cbbecf40c1;
         using Py = import "/capnpy/annotate.capnp";
         struct Foo {
             x :group $Py.nullable {
-                wrongName @0 :Int8;
+                wrongName @0 :Bool;
                 value  @1 :Int64;
             }
         }
@@ -165,7 +164,6 @@ class TestNullable(CompilerTest):
         msg = str(exc.value)
         assert msg == ('x: nullable groups must have exactly two fields: '
                        '"isNull" and "value"')
-
 
     def test_bad_nullable_2(self):
         schema = """
@@ -182,3 +180,39 @@ class TestNullable(CompilerTest):
         msg = str(exc.value)
         assert msg == ('x: cannot use pointer types for nullable values. '
                        'Pointers are already nullable.')
+
+    def test_default_nonnull(self):
+        schema = """
+        @0xbf5147cbbecf40c1;
+        using Py = import "/capnpy/annotate.capnp";
+        struct Foo {
+            x :group $Py.nullable {
+                isNull @0 :Bool;
+                value  @1 :Int64 = 42;
+            }
+        }
+        """
+        mod = self.compile(schema)
+        foo = mod.Foo()
+        assert foo.x == 42
+        assert foo._seg.buf == (
+            '\x00\x00\x00\x00\x00\x00\x00\x00'  # 0
+            '\x00\x00\x00\x00\x00\x00\x00\x00') # 0 (value == 42)
+
+    def test_default_null(self):
+        schema = """
+        @0xbf5147cbbecf40c1;
+        using Py = import "/capnpy/annotate.capnp";
+        struct Foo {
+            x :group $Py.nullable {
+                isNull @0 :Bool = true;
+                value  @1 :Int64;
+            }
+        }
+        """
+        mod = self.compile(schema)
+        foo = mod.Foo()
+        assert foo.x is None
+        assert foo._seg.buf == (
+            '\x00\x00\x00\x00\x00\x00\x00\x00'  # 0 (isNull == true)
+            '\x00\x00\x00\x00\x00\x00\x00\x00') # 0

commit e17ec368ed1fb4094f6f63f9d6bf88ffc7732ced

fix constructors when using a $Py.nullable with a group value; this was broken by b55d4bc

capnpy/compiler/fieldtree.py
@@ -96,7 +96,10 @@ class Node(AbstractNode):
         if prefix:
             self.varname = '%s_%s' % (prefix, self.varname)
         self._init_children(m, field_force_default)
-        self._init_default(m)
+        #
+        # self.default is a *string* containing a Python repr of the default
+        # value
+        self.default = self._get_default(m)
 
     def _init_children(self, m, field_force_default):
         self.children = []
@@ -107,29 +110,27 @@ class Node(AbstractNode):
             if group.struct.is_union():
                 self.union = Union(self.varname, group.struct.discriminantOffset*2)
 
-    def _init_default(self, m):
-        # self.default is a *string* containing a Python repr of the default
-        # value
-        def get_default(f):
-            if f.is_part_of_union() and not self.force_default:
-                return '_undefined'
-            elif f.is_slot():
-                default_val = f.slot.defaultValue.as_pyobj()
-                return str(default_val)
-            elif f.is_nullable(m):
-                ann = f.is_nullable(m)
-                name, f_is_null, f_value = ann.check(m)
-                default_is_null = f_is_null.slot.defaultValue.as_pyobj()
-                if default_is_null:
-                    return 'None'
-                else:
-                    return get_default(f_value)
+    def _get_default(self, m):
+        f = self.f
+        if f.is_part_of_union() and not self.force_default:
+            return '_undefined'
+        elif f.is_slot():
+            default_val = f.slot.defaultValue.as_pyobj()
+            return str(default_val)
+        elif f.is_nullable(m):
+            ann = f.is_nullable(m)
+            name, f_is_null, f_value = ann.check(m)
+            default_is_null = f_is_null.slot.defaultValue.as_pyobj()
+            if default_is_null:
+                return 'None'
             else:
-                assert f.is_group()
-                items = [child.default for child in self.children]
-                return '(%s,)' % ', '.join(items)
+                # self.children[1] is the Node for "value" field of the group
+                return self.children[1].default
+        else:
+            assert f.is_group()
+            items = [child.default for child in self.children]
+            return '(%s,)' % ', '.join(items)
 
-        self.default = get_default(self.f)
 
     def __repr__(self):
         return '<Node %s: %s>' % (self.varname, self.f.which())

capnpy/testing/compiler/test_null.py
@@ -213,6 +213,28 @@ class TestNullable(CompilerTest):
             '\x00\x00\x00\x00\x00\x00\x00\x00'  # 0
             '\x00\x00\x00\x00\x00\x00\x00\x00') # 0 (value == 42)
 
+    def test_default_nonnull_group(self):
+        schema = """
+        @0xbf5147cbbecf40c1;
+        using Py = import "/capnpy/annotate.capnp";
+        struct Foo {
+            point :group $Py.nullable {
+                isNull @0 :Bool;
+                value :group {
+                    x @1: Int8 = 4;
+                    y @2: Int8 = 5;
+                    z @3: Int8 = 6;
+                }
+            }
+        }
+        """
+        mod = self.compile(schema)
+        foo = mod.Foo()
+        assert foo.point.x == 4
+        assert foo.point.y == 5
+        assert foo.point.z == 6
+        assert foo._seg.buf == b('\x00\x00\x00\x00\x00\x00\x00\x00')
+
     def test_default_null(self):
         schema = """
         @0xbf5147cbbecf40c1;
@@ -230,3 +252,23 @@ class TestNullable(CompilerTest):
         assert foo._seg.buf == b(
             '\x00\x00\x00\x00\x00\x00\x00\x00'  # 0 (isNull == true)
             '\x00\x00\x00\x00\x00\x00\x00\x00') # 0
+
+    def test_default_null_group(self):
+        schema = """
+        @0xbf5147cbbecf40c1;
+        using Py = import "/capnpy/annotate.capnp";
+        struct Foo {
+            point :group $Py.nullable {
+                isNull @0 :Bool = true;
+                value :group {
+                    x @1: Int8;
+                    y @2: Int8;
+                    z @3: Int8;
+                }
+            }
+        }
+        """
+        mod = self.compile(schema)
+        foo = mod.Foo()
+        assert foo.point is None
+        assert foo._seg.buf == b('\x00\x00\x00\x00\x00\x00\x00\x00')