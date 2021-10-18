862cda7607657d5d5831a221076f9fb3ec50a567
Antonio Cuni <anto.cuni@gmail.com>
2021-10-18 15:24:08 +0200

    try to cache capnproto between runs

---
+++ ci/install_capnp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# $CAPNPROTO is defined by test.yml env
+
+if [ ! -d capnproto ]; then
+    echo 'Compiling capnproto'
+    curl -O https://capnproto.org/$CAPNPROTO.tar.gz
+    tar zxf $CAPNPROTO.tar.gz
+    mv $CAPNPROTO capnproto
+    cd capnproto
+    ./configure
+    make -j8
+else
+    echo 'Using cached capnproto'
+    cd capnproto
+fi
+
+sudo make install
+capnp --version
